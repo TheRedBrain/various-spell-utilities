91413b791c0b3aea9aa53d83ed67a43a99961f4c
TheRedBrain <thetrueredbrain@gmail.com>
2024-10-19 11:18:53 +0200

    setup

--- src/client/java/com/github/theredbrain/variousspells/VariousSpellsClient.java
+++ src/client/java/com/github/theredbrain/variousspells/VariousSpellsClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.github.theredbrain.variousspells;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class VariousSpellsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
